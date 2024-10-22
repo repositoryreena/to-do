41b7ce0a2646fc0f41609b6fc8165f11aea1463f
Reena Thomas <rtthomas@ucsc.edu>
2024-10-22 11:48:51 -0700

    initial commit

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 document.getElementById("add").addEventListener("click", ()=>{
 
 
@@ -13,23 +11,6 @@ document.getElementById("add").addEventListener("click", ()=>{
     </li>
     `
 
-    // const newSpan = document.createElement("span")
-    // const newLi = document.createElement("li")
-    // const newInput = document.createElement("input")
-    // const newInput2 = document.createElement("input")
-    // const newBtn = document.createElement("button")
-
-    // newInput.value = `${document.getElementById("input").value}`
-    // newBtn.textContent = "Delete"
-    // newInput2.type="checkbox"
-    // newBtn.id = "delete"
-    // newBtn.classList.add("delete")
-
-    // document.getElementById("container").appendChild(newSpan)
-    // document.getElementById("container").appendChild(newLi)
-    // document.getElementById("container").appendChild(newInput)
-    // document.getElementById("container").appendChild(newInput2)
-    // document.getElementById("container").appendChild(newBtn)
 
 
     document.querySelectorAll(".delete").forEach(element=>element.addEventListener("click", (event)=>{
@@ -38,27 +19,14 @@ document.getElementById("add").addEventListener("click", ()=>{
             listItem.remove();
           }
 
-        // location.reload()
         }))
 
-// const newButton = document.querySelector("button")
-// newButton.id = "delete"
 })
 
 
 
     
     
-//
-
-
-
-// delete2.addEventListener("click", (event)=>{
-//     if (event.target.classList.contains('delete')) {
-//         const listItem = event.target.parentElement;
-//         listItem.remove();
-//       }
-// })
 
 
 
@@ -67,9 +35,3 @@ document
             .addEventListener("click", function (event) {
                 event.preventDefault();
             });
-
-            // document
-            // .getElementById("delete")
-            // .addEventListener("click", function (event) {
-            //     event.preventDefault();
-            // });
